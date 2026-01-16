73612c2ef937842dc17aeefd440e2b2bb59a8dce
John Orthoefer <jco@orthoefer.org>
2026-01-16 12:05:15 -0500

    Update session_header.h to define session_start_header_t structure

---
+++ components/logging/include/session_header.h
@@ -0,0 +1,23 @@
+#ifndef SESSION_HEADER_H
+#define SESSION_HEADER_H
+
+#include <stdint.h>
+
+#define SESSION_START_MAGIC 0x53545230 // 'STR0'
+#define SESSION_FORMAT_VERSION 0x01
+
+typedef struct __attribute__((packed)) {
+    uint32_t magic;             // SESSION_START_MAGIC
+    uint8_t  version;           // format version
+    uint8_t  reserved[3];
+    uint8_t  startup_id[16];    // UUIDv4 for this session
+    int64_t  esp_time_at_start; // esp_timer_get_time() at startup (µs)
+    int64_t  gps_utc_at_lock;   // seconds since epoch (0 if unknown)
+    uint8_t  mac_addr[6];       // device MAC (efuse)
+    uint8_t  fw_sha[8];         // short git SHA (8 bytes)
+    uint32_t startup_counter;   // monotonic counter persisted in NVS (optional)
+    uint32_t reserved2[2];      // for future use
+    uint32_t crc32;             // CRC32 of the header (exclude this field)
+} session_start_header_t;
+
+#endif // SESSION_HEADER_H
